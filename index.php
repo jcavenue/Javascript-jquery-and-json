31fba8abc3f7db0e6927dae8a4c88b362da97f54
Suzaku <jcfababeir09@gmail.com>
2021-06-21 16:16:35 +0800

    Update Title tag

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>e5688b71</title>
+		<title>17a8d311</title>
 		<!-- Bootstrap 5 Beta Minified CSS -->
 		<link rel="stylesheet" href="css/bootstrap.min.css">
 		<!-- Bootstrap 5 Beta Minified JS -->
